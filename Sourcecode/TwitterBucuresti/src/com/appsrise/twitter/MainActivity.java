7b71fbeff722e956560452290a1746276426283e
Marius Mailat <marius.mailat@appseleration.com>
2013-08-02 16:11:24 +0200

    + preferinte

--- Sourcecode/TwitterBucuresti/src/com/appsrise/twitter/MainActivity.java
+++ Sourcecode/TwitterBucuresti/src/com/appsrise/twitter/MainActivity.java
@@ -2,9 +2,13 @@ package com.appsrise.twitter;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -19,16 +23,23 @@ import android.widget.Toast;
 
 import com.marakana.android.yamba.clientlib.YambaClient;
 
-public class MainActivity extends Activity implements OnClickListener{
+public class MainActivity extends Activity implements OnClickListener, OnPreferenceChangeListener{
 	EditText editText;
 	Button updateButton;
 	TextView textCount;
-	
+	SharedPreferences prefs;
+	String username;
+	String password;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//prepare the preferences
+		prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		username = prefs.getString("username", "student");
+		password = prefs.getString("password", "password");
 		
 		//obtine textul din edit text
 		editText = (EditText) findViewById(R.id.editText);
@@ -90,8 +101,10 @@ public class MainActivity extends Activity implements OnClickListener{
 
 		@Override
 		protected String doInBackground(String... params) {
+
+			
 			//posteaza textul pe twitter server
-			YambaClient yambaClient = new YambaClient("student", "password");
+			YambaClient yambaClient = new YambaClient(username, password);
 			try {
 				yambaClient.postStatus(editText.getText().toString());
 				Log.d("TwitterBucuresti", "We just posted: " + editText.getText().toString());
@@ -112,6 +125,14 @@ public class MainActivity extends Activity implements OnClickListener{
 		protected void onProgressUpdate(Integer... values) {
 			super.onProgressUpdate(values);
 		}	
+	}
+
+	@Override
+	public boolean onPreferenceChange(Preference preference, Object newValue) {
+		//read from preferences
+		username = prefs.getString("username", "student");
+		password = prefs.getString("password", "password");
+		return false;
 	}
 
 }
